f0c2895a3122e1ef3115bbb83b6a641de98aaa1b
IyinOlu-Dev <olutomiwa.oyegbola@gmail.com>
2026-09-18 13:55:25 -0500

    added backend api to send data(incomplete)

--- templates/script.js
+++ templates/script.js
@@ -2,21 +2,27 @@ document.getElementById('getLocationBtn').addEventListener('click', function() {
     if (!navigator.geolocation) {
         alert("Geolocation is not supported by your browser");
         return;
-    }
+        }
 
     navigator.geolocation.getCurrentPosition(
         (position) => {
             const latitude = position.coords.latitude;
             const longitude = position.coords.longitude;
             console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-        },
+            },
         (error) => {
             console.error("Error getting location:", error.message);
-        },
-        {
+            },
+            {
             enableHighAccuracy: true,
             timeout: 10000,
             maximumAge: 0
+            }
+            
+        );
+
+        try{
+            const response = await fetch(`https://https://checkin-vmcb.onrender.com/`)
         }
-    );
-});
+    }
+);
