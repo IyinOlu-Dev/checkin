f9394263bbc83cd0c8445f6aaecfa49a12507cfb
IyinOlu-Dev <olutomiwa.oyegbola@gmail.com>
2026-09-18 15:07:44 -0500

    made clockin variable

--- main.py
+++ main.py
@@ -27,17 +27,36 @@ async def home():
 @app.post("/api/location",)
 async def locate(data: LocationData):
 
-
     longitude = data.lng
     latitude = data.lat
 
+    ckeckin = False
     print (f" Received Location -> latitude: {latitude} & Longitude : {longitude} ")
+
+    if (-87.9 <= longitude <= -87.7) and (41.8 <= latitude <= 42.0):
+        checkin = True
+    else:
+        checkin = False
+
 
     return {
         "status" : "Successful",
         "message": "Location reached successfully",
         "latitude" : latitude,
-        "longitude" : longitude
+        "longitude" : longitude,
+        "checkin_status" : checkin 
     }
 
+
+
+
         # 41.97779999064488, -87.71722227283024
+
+
+        # 41.86204973667799, -87.8185480677973
+
+        # 41.862145620691884, -87.81690655585933
+
+        # 41.861490410398, -87.81837640641818
+        
+        # 41.86150639121677, -87.81683145400596
